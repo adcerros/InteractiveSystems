03fdcf22695cf9a5207f4c56e5c95fad56cce873
Adrian Cerros <adriancerrossanchez@gmail.com>
2022-02-21 05:24:38 +0100

    refactorizacion codigo

--- MiniPracticaUno/src/app/app.component.ts
+++ MiniPracticaUno/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, EventEmitter, Output, HostListener} from '@angular/core';
+import { Component, ViewChild, ElementRef} from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { MyComponentLoaderDirective } from '../app/myComponentCreator'
 
@@ -14,14 +14,58 @@ export class AppComponent {
 @Component({
   selector: 'mainDiv',
   template: '<div class="mainDiv">\
-              <billMaker (uppParentCounter)="upCounter($event)" (downParentCounter)="downCounter($event)"></billMaker>\
-              <div totalBill=totalBill>El total es: {{totalBill}}</div>\
+              <billMaker></billMaker>\
             </div>',
   styleUrls: ['./app.component.scss']
 })
 
 export class mainDiv {  
-  totalBill = 0;
+}
+
+@Component({
+  selector: 'billMaker',
+  template: '\
+    <select id=selectionList>\
+      <option>Selecciona el plato a añadir</option>\
+      <option>Salmon a la plancha</option>\
+      <option>Carrillada al vino</option>\
+      <option>Chuleton de buey</option>\
+      <option>Tartar de atun</option>\
+      <option>Solomillo al oporto</option>\
+    </select>\
+    <button (click)=upBill()>Add</button>\
+    <ng-template dishDinamicComponentHost></ng-template>\
+    <div totalBill=totalBill>El total es: {{totalBill}}</div>',
+  styleUrls: ['./app.component.scss']
+})
+
+export class billMaker {  
+  dishList : Array<string>;
+  pricesList : Array<number>;
+  totalBill : number;
+  @ViewChild(MyComponentLoaderDirective) dynamicHost !: MyComponentLoaderDirective;
+  constructor(){
+    this.dishList = ["Salmon a la plancha", "Carrillada al vino", "Chuleton de buey", "Tartar de atun", "Solomillo al oporto"];
+    this.pricesList = [11, 14, 9, 16, 17];
+    this.totalBill = 0;
+  }
+
+  upBill(): void{
+    var dropdown = document.getElementById('selectionList') as HTMLSelectElement;
+    if (dropdown.selectedIndex > 0){
+      let realSelectedIndex = dropdown.selectedIndex - 1;
+      const newComponent = this.dynamicHost.viewContainerRef.createComponent(dishComponent);
+      newComponent.instance.price = this.pricesList[realSelectedIndex];
+      newComponent.instance.dishName = this.dishList[realSelectedIndex];
+      newComponent.instance.downParentCounters$Obs.subscribe(price =>{
+        this.totalBill = this.totalBill - price;
+      })
+      this.upCounter(this.pricesList[realSelectedIndex])
+    }
+    else{
+      alert("Selecciona el plato a añadir");
+    }
+  }
   downCounter(newPrice: any): void{
     this.totalBill = this.totalBill - newPrice;
   }
@@ -32,46 +76,8 @@ export class mainDiv {
 }
 
 @Component({
-  selector: 'billMaker',
-  template: '\
-    <select id=selectionList>\
-      <option>Salmon a la plancha</option>\
-      <option>Carrillada al vino</option>\
-      <option>Chuleton de buey</option>\
-      <option>Tartar de atun</option>\
-      <option>Solomillo al oporto</option>\
-    </select>\
-    <button (click)=upBill()>Add</button>\
-    <ng-template dishDinamicComponentHost></ng-template>',
-  styleUrls: ['./app.component.scss']
-})
-
-export class billMaker {  
-  dishList = ["Salmon a la plancha", "Carrillada al vino", "Chuleton de buey", "Tartar de atun", "Solomillo al oporto"];
-  pricesList = [11, 14, 9, 16, 17]
-  @ViewChild(MyComponentLoaderDirective) dynamicHost !: MyComponentLoaderDirective;
-  @Output() uppParentCounter = new EventEmitter<any>();
-  @Output() downParentCounter = new EventEmitter<any>();
-  constructor(){
-    this.uppParentCounter = new EventEmitter();
-    this.downParentCounter = new EventEmitter();
-  }
-
-  upBill(): void{
-    var dropdown = document.getElementById('selectionList') as HTMLSelectElement;
-    this.uppParentCounter.emit(this.pricesList[dropdown.selectedIndex]);
-    const newComponent = this.dynamicHost.viewContainerRef.createComponent(dishComponent);
-    newComponent.instance.price = this.pricesList[dropdown.selectedIndex];
-    newComponent.instance.dishName = this.dishList[dropdown.selectedIndex];
-    newComponent.instance.downParentCounters$Obs.subscribe(price =>{
-      this.downParentCounter.emit(price);
-    })
-  }
-}
-
-@Component({
   selector: 'dishComponent',
-  template: '<div dishName="dishName" price="price">{{dishName}}{{price}}</div><button (click)="deleteDish()">Delete</button>',
+  template: '<div dishName="dishName" price="price">{{dishName}}  {{price}}</div><button (click)="deleteDish()">Delete</button>',
   styleUrls: ['./app.component.scss'],
   providers: [
     {provide: 'price', useValue: 'container'},
@@ -79,11 +85,13 @@ export class billMaker {
 })
 
 export class dishComponent {  
-  dishName = "undefined";
-  price = 0;
+  dishName : string;
+  price : number;
   downParentCounter$: Subject<number>;
-  downParentCounters$Obs: Observable<Number>;
+  downParentCounters$Obs: Observable<number>;
   constructor(private hostComponent: ElementRef<HTMLElement>){
+    this.dishName = "undefined";
+    this.price = 0;
     this.downParentCounter$ = new Subject();
     this.downParentCounters$Obs = this.downParentCounter$.asObservable();
   }
